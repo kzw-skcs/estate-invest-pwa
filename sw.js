30fd902f2a7d1caa0f22aab27d4383d759d3885a
Shuichiro Kazawa <kazawa@skclinicalsupport.com>
2026-08-13 01:25:35 +0900

    初回コミット: 負けない不動産投資プランナー PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'estate-invest-v1';
+const CACHE_FILES = [
+  './', './index.html', './manifest.json',
+  './icon-192.png', './icon-512.png', './icon-180.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(CACHE_FILES).catch(err => console.warn('Cache partial fail:', err))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (event.request.url.includes('index.html') || event.request.url.endsWith('/')) {
+        return fetch(event.request)
+          .then(res => {
+            caches.open(CACHE_NAME).then(c => c.put(event.request, res.clone()));
+            return res;
+          }).catch(() => cached);
+      }
+      return cached || fetch(event.request);
+    })
+  );
+});
